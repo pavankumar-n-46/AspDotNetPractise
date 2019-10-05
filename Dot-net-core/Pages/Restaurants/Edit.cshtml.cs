3a6d5f1d8fd22e1bb346514c27c27c961a0ed6c6
N, Pavan Kumar <pn@allegion.com>
2019-10-05 22:41:19 +0530

    Building Edit Form with form helpers

--- Dot-net-core/Pages/Restaurants/Edit.cshtml.cs
+++ Dot-net-core/Pages/Restaurants/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.data;
 
@@ -12,15 +13,20 @@ namespace Dot_net_core.Pages.Restaurants
     public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
 
         public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
 
-        public EditModel(IRestaurantData restaurantData)
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
         }
         public IActionResult OnGet(int restaurantID)
         {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+
             Restaurant = restaurantData.GetRestaurantById(restaurantID);
 
             if(Restaurant != null)
