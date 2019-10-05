7cb9bb450f1a2b00056a8496de2d1d34d0249e83
N, Pavan Kumar <pn@allegion.com>
2019-10-05 21:34:29 +0530

    Creating a restaurant edit page

---
+++ Dot-net-core/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.data;
+
+namespace Dot_net_core.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public Restaurant Restaurant { get; set; }
+
+        public EditModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int restaurantID)
+        {
+            Restaurant = restaurantData.GetRestaurantById(restaurantID);
+
+            if(Restaurant != null)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./NotFound");
+        }
+    }
+}
