b196a54fb69bf507cc5e4975c9da495bd1f69e5f
N, Pavan Kumar <pn@allegion.com>
2019-10-08 17:39:59 +0530

    Implement EF DbContext, Using EF tools

---
+++ OdeToFood.data/OdeToFoodDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.data
+{
+    class OdeToFoodDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
